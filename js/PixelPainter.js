f0b91aa3b1a5dcca44e244c10fddafda0121430b
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-07-07 19:33:03 -1000

    Created color palette, canvas, now adding in drag function

---
+++ js/PixelPainter.js
@@ -0,0 +1,66 @@
+var colorDiv = document.createElement("div");
+colorDiv.id = "colorDiv";
+document.body.appendChild(colorDiv);
+
+for (var i=0; i<9; i++) {
+    var makeColorRow = document.createElement("div");
+    makeColorRow.className = "colorRow";
+    colorDiv.appendChild(makeColorRow);
+}
+var colorRow = document.getElementsByClassName("colorRow");
+
+for (var i=0; i<colorRow.length; i++) {
+    for (var j=0; j<8; j++) {
+        var makeColorSquare = document.createElement("div");
+        makeColorSquare.className = "colorSquare";
+        makeColorSquare.addEventListener("click", selectColor);
+        colorRow[i].appendChild(makeColorSquare);
+    }
+}
+
+var colorSquare = document.getElementsByClassName("colorSquare");
+colorSquare[0].style.border = "2px solid yellow"
+
+var currentColor = colorSquare[0].style.backgroundColor;
+
+function selectColor() {
+    for (var i=0; i<colorSquare.length; i++) {
+        if (colorSquare[i].style.border === "2px solid yellow") {
+            colorSquare[i].style.border = "1px solid green";
+        }
+    }
+    currentColor = this.style.backgroundColor;
+    console.log(currentColor);
+    this.style.border = "2px solid yellow";
+}
+
+var colorArr = ["indianred", "lightcoral", "salmon", "crimson", "red", "firebrick", "darkred", "maroon", "coral", "tomato", "orangered", "darkorange", "orange", "gold", "yellow", "sandybrown", "goldenrod", "darkgoldenrod", "khaki", "lime", "springgreen", "mediumseagreen", "seagreen", "forestgreen", "green", "darkgreen", "olivedrab", "lightseagreen", "darkcyan", "teal", "aqua", "turquoise", "mediumturquoise", "steelblue", "skyblue", "deepskyblue", "dodgerblue", "cornflowerblue", "blue", "mediumblue", "darkblue", "midnightblue", "darkslateblue", "slateblue", "indigo", "rebeccapurple", "purple", "darkorchid", "darkviolet", "blueviolet", "mediumorchid", "magenta", "orchid", "violet", "plum", "pink", "lightpink", "hotpink", "deeppink", "mediumvioletred", "burlywood", "tan", "rosybrown", "peru", "chocolate", "saddlebrown", "sienna", "brown", "black", "grey", "lightgrey", "white"]
+
+for (var i=0; i<colorArr.length; i++) {
+    colorSquare[i].style.backgroundColor = colorArr[i];
+}
+
+var canvasDiv = document.createElement("div");
+canvasDiv.id = "canvasDiv";
+document.body.appendChild(canvasDiv);
+
+for (var i=0; i<20; i++) {
+    var makeCanvasRow = document.createElement("div");
+    makeCanvasRow.className = "canvasRow";
+    canvasDiv.appendChild(makeCanvasRow);
+}
+
+var canvasRow = document.getElementsByClassName("canvasRow");
+
+for (var i=0; i<canvasRow.length; i++) {
+    for (var j=0; j<20; j++) {
+        var makeCanvasSquare = document.createElement("div");
+        makeCanvasSquare.className = "canvasSquare";
+        makeCanvasSquare.addEventListener("mousedown", paint);
+        canvasRow[i].appendChild(makeCanvasSquare);
+    }
+}
+
+function paint() {
+    this.style.backgroundColor = currentColor;   
+}
