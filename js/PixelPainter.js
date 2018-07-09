79ca239b9b40f8215ee06d7fc26ba9aafaec7c70
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.hawaii.rr.com>
2018-07-08 19:14:39 -1000

    Prepping for gh-pages

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,7 +2,7 @@ var colorDiv = document.createElement("div");
 colorDiv.id = "colorDiv";
 document.body.appendChild(colorDiv);
 
-for (var i=0; i<9; i++) {
+for (var i=0; i<10; i++) {
     var makeColorRow = document.createElement("div");
     makeColorRow.className = "colorRow";
     colorDiv.appendChild(makeColorRow);
@@ -19,9 +19,16 @@ for (var i=0; i<colorRow.length; i++) {
 }
 
 var colorSquare = document.getElementsByClassName("colorSquare");
-colorSquare[0].style.border = "2px solid yellow"
+colorSquare[0].style.border = "2px solid yellow";
+
+var colorArr = ["indianred", "lightcoral", "salmon", "crimson", "red", "firebrick", "darkred", "maroon", "coral", "tomato", "orangered", "darkorange", "orange", "gold", "yellow", "sandybrown", "goldenrod", "darkgoldenrod", "khaki", "lime", "springgreen", "mediumseagreen", "seagreen", "forestgreen", "green", "darkgreen", "olivedrab", "lightseagreen", "darkcyan", "teal", "aqua", "turquoise", "mediumturquoise", "steelblue", "skyblue", "deepskyblue", "dodgerblue", "cornflowerblue", "blue", "mediumblue", "darkblue", "midnightblue", "darkslateblue", "slateblue", "indigo", "rebeccapurple", "purple", "darkorchid", "darkviolet", "blueviolet", "mediumorchid", "magenta", "orchid", "violet", "plum", "pink", "lightpink", "hotpink", "deeppink", "mediumvioletred", "burlywood", "tan", "rosybrown", "peru", "chocolate", "saddlebrown", "sienna", "brown", "black", "grey", "lightgrey", "white"]
+
+for (var i=0; i<colorArr.length; i++) {
+    colorSquare[i].style.backgroundColor = colorArr[i];
+}
 
 var currentColor = colorSquare[0].style.backgroundColor;
+console.log(currentColor)
 
 function selectColor() {
     for (var i=0; i<colorSquare.length; i++) {
@@ -32,12 +39,6 @@ function selectColor() {
     currentColor = this.style.backgroundColor;
     console.log(currentColor);
     this.style.border = "2px solid yellow";
-}
-
-var colorArr = ["indianred", "lightcoral", "salmon", "crimson", "red", "firebrick", "darkred", "maroon", "coral", "tomato", "orangered", "darkorange", "orange", "gold", "yellow", "sandybrown", "goldenrod", "darkgoldenrod", "khaki", "lime", "springgreen", "mediumseagreen", "seagreen", "forestgreen", "green", "darkgreen", "olivedrab", "lightseagreen", "darkcyan", "teal", "aqua", "turquoise", "mediumturquoise", "steelblue", "skyblue", "deepskyblue", "dodgerblue", "cornflowerblue", "blue", "mediumblue", "darkblue", "midnightblue", "darkslateblue", "slateblue", "indigo", "rebeccapurple", "purple", "darkorchid", "darkviolet", "blueviolet", "mediumorchid", "magenta", "orchid", "violet", "plum", "pink", "lightpink", "hotpink", "deeppink", "mediumvioletred", "burlywood", "tan", "rosybrown", "peru", "chocolate", "saddlebrown", "sienna", "brown", "black", "grey", "lightgrey", "white"]
-
-for (var i=0; i<colorArr.length; i++) {
-    colorSquare[i].style.backgroundColor = colorArr[i];
 }
 
 var canvasDiv = document.createElement("div");
@@ -57,10 +58,23 @@ for (var i=0; i<canvasRow.length; i++) {
         var makeCanvasSquare = document.createElement("div");
         makeCanvasSquare.className = "canvasSquare";
         makeCanvasSquare.addEventListener("mousedown", paint);
+        makeCanvasSquare.addEventListener("mouseup", stopPaint);
+        makeCanvasSquare.addEventListener("mouseover", dragPaint);
         canvasRow[i].appendChild(makeCanvasSquare);
     }
 }
 
 function paint() {
-    this.style.backgroundColor = currentColor;   
+    this.style.backgroundColor = currentColor;
+    console.log(this);  
+}
+
+function stopPaint() {
+    if (paint() === true) {
+        return;
+    }
+}
+
+function dragPaint() {
+    
 }
