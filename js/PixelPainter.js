2c20e2a93542e1fcfb1c9bd4d5f1c3451780c685
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.hawaii.rr.com>
2018-07-10 00:29:58 -1000

    Added more CSS, clear, erase, save, load buttons

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,6 +1,12 @@
+var pixelPainter = document.getElementById("pixelPainter");
+
+var options = document.createElement("div");
+options.id = "options";
+pixelPainter.appendChild(options);
+
 var colorDiv = document.createElement("div");
 colorDiv.id = "colorDiv";
-document.body.appendChild(colorDiv);
+options.appendChild(colorDiv);
 
 for (var i=0; i<10; i++) {
     var makeColorRow = document.createElement("div");
@@ -30,6 +36,8 @@ for (var i=0; i<colorArr.length; i++) {
 var currentColor = colorSquare[0].style.backgroundColor;
 console.log(currentColor)
 
+var eraseStatus = false;
+
 function selectColor() {
     for (var i=0; i<colorSquare.length; i++) {
         if (colorSquare[i].style.border === "2px solid yellow") {
@@ -39,11 +47,36 @@ function selectColor() {
     currentColor = this.style.backgroundColor;
     console.log(currentColor);
     this.style.border = "2px solid yellow";
+    eraseStatus = false;
+}
+
+var buttonsDiv = document.createElement("div");
+buttonsDiv.id = "buttonsDiv";
+options.appendChild(buttonsDiv);
+
+var eraseButton = document.createElement("div");
+eraseButton.id = "eraseButton";
+eraseButton.innerHTML = "ERASE";
+eraseButton.addEventListener("click", eraseTool);
+buttonsDiv.appendChild(eraseButton);
+
+function eraseTool() {
+    eraseStatus = true;
+}
+
+var clearButton = document.createElement("div");
+clearButton.id = "clearButton";
+clearButton.innerHTML = "CLEAR";
+clearButton.addEventListener("click", areYouSure);
+buttonsDiv.appendChild(clearButton);
+
+function areYouSure() {
+    areYouSure.style.display = "block";
 }
 
 var canvasDiv = document.createElement("div");
 canvasDiv.id = "canvasDiv";
-document.body.appendChild(canvasDiv);
+pixelPainter.appendChild(canvasDiv);
 
 for (var i=0; i<20; i++) {
     var makeCanvasRow = document.createElement("div");
@@ -58,18 +91,93 @@ for (var i=0; i<canvasRow.length; i++) {
         var makeCanvasSquare = document.createElement("div");
         makeCanvasSquare.className = "canvasSquare";
         makeCanvasSquare.addEventListener("mousedown", paint);
-        makeCanvasSquare.addEventListener("mouseup", stopPaint);
         canvasRow[i].appendChild(makeCanvasSquare);
     }
 }
 
-function paint() {
-    this.style.backgroundColor = currentColor;
-    console.log(this);  
+var canvasSquare = document.getElementsByClassName("canvasSquare");
+
+function paintMore() {
+    if (eraseStatus === false) {
+        this.style.backgroundColor = currentColor;
+        console.log(this);
+    } else {
+        this.style.backgroundColor = null;
+    } 
 }
 
-function stopPaint() {
-    if (paint() === true) {
-        return;
+function paint() {
+    if (eraseStatus === false) {
+        this.style.backgroundColor = currentColor;
+        console.log(this);
+    } else {
+        this.style.backgroundColor = null;
+    } 
+}
+
+var areYouSure = document.createElement("div");
+areYouSure.id = "areYouSure";
+areYouSure.innerHTML = "ARE YOU SURE?"
+document.body.appendChild(areYouSure);
+areYouSure.style.display = "none";
+
+var yesButton = document.createElement("div");
+yesButton.id = "yesButton";
+yesButton.innerHTML = "YES";
+yesButton.addEventListener("click", clearCanvas);
+areYouSure.appendChild(yesButton);
+
+function clearCanvas() {
+    for (var i=0; i<canvasSquare.length; i++) {
+        canvasSquare[i].style.backgroundColor = null;
+    }
+    if (areYouSure.style.display === "block") {
+        areYouSure.style.display = "none";
+    }
+}
+
+var noButton = document.createElement("div");
+noButton.id = "noButton";
+noButton.innerHTML = "NO";
+noButton.addEventListener("click", closeMenu);
+areYouSure.appendChild(noButton);
+
+function closeMenu() {
+    areYouSure.style.display = "none";
+}
+
+var saveLoadDiv = document.createElement("div");
+saveLoadDiv.id = "saveLoadDiv";
+options.appendChild(saveLoadDiv);
+
+var saveButton = document.createElement("div");
+saveButton.id = "saveButton";
+saveButton.innerHTML = "SAVE";
+saveButton.addEventListener("click", savePic);
+saveLoadDiv.appendChild(saveButton);
+
+var loadButton = document.createElement("div");
+loadButton.id = "loadButton"; 
+loadButton.innerHTML = "LOAD";
+loadButton.addEventListener("click", loadPic);
+saveLoadDiv.appendChild(loadButton);
+
+var saveArr = [];
+function savePic() {
+    saveArr = [];
+    for (var i=0; i<canvasSquare.length; i++) {
+        saveArr.push(canvasSquare[i].style.backgroundColor);
+    }
+    console.log(saveArr);
+    console.log("Pic Saved!");
+}
+
+function loadPic() {
+    if ((saveArr)) {
+        for (var i=0; i<canvasSquare.length; i++) {
+            canvasSquare[i].style.backgroundColor = saveArr[i];
+        }
+    } else {
+        console.log("Nothing to load");
     }
 }
